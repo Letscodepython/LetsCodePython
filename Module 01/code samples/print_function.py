f10abbff9bd0874d3039048287730c4ae1f0ad9b
LetsCodePython <pythonletscode@gmail.com>
2022-04-18 22:09:02 +0200

    Initial commit

---
+++ Module 01/code samples/print_function.py
@@ -0,0 +1,7 @@
+print('Python rules!')
+print('Python is cool!')
+print('Python rules', 'Python is cool')
+
+# print(f'Python rules!')
+# print(f'Python is cool!')
+# print(f'Python rules Python is cool')
